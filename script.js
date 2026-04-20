b9b6cf4d0900541e2a8b591ce2b3a94519fad121
Opsolver120 <akshatsolver@gmail.com>
2026-04-20 17:23:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+async function loadGuilds() {
+  const res = await fetch("http://85.215.229.230:9389/guilds");
+  const data = await res.json();
+
+  const container = document.getElementById("guilds");
+  container.innerHTML = "";
+
+  data.guilds.forEach(g => {
+    const div = document.createElement("div");
+    div.className = "card";
+    div.innerHTML = `<h3>${g.name}</h3><p>ID: ${g.id}</p>`;
+    container.appendChild(div);
+  });
+}
